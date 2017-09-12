a969538245761b736540b4a4a46d5dd6cf61a2a2
mack94 <makowka@student.agh.edu.pl>
2017-09-12 17:52:11 +0200

    tiger walking vol 2 idle with catch and sound.

--- Assets/Scripts/TigerAnim.cs
+++ Assets/Scripts/TigerAnim.cs
@@ -11,14 +11,18 @@ public class TigerAnim : MonoBehaviour {
     public float stepLenght = 0.01f;
     [Tooltip("The state name for tiger walking. Name could be got from the tiger Animator screen. Default name is `walk`")]
     public String animatorStateNameForWalk = "walk";
+    [Tooltip("Locking the tiger rotation. This prevents tiger from falling in X and Z axis. Default is true - enabled protection.")]
+    public bool lockRotationXZ = true;
 
     private Animator animator;
     private float walking;
-    private AnimatorStateInfo animatorState; 
+    private AnimatorStateInfo animatorState;
+    private Rigidbody rb;
 
     // Use this for initialization
     void Start () {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
         walking = 0.0f;
     }
 	
@@ -28,6 +32,7 @@ public class TigerAnim : MonoBehaviour {
         animatorState = animator.GetCurrentAnimatorStateInfo(0);
         if (animatorState.IsName(animatorStateNameForWalk))
         {
+            /* our old script
             float move_x;
             float move_y;
             float angle;
@@ -37,7 +42,24 @@ public class TigerAnim : MonoBehaviour {
             rad = angle * (Math.PI / 180);
             move_x = Convert.ToSingle(stepLenght / Math.Pow(Math.Sqrt(1.0 + Math.Tan(rad)), 2));
             move_y = Convert.ToSingle(move_x * Math.Tan(rad));
-            transform.Translate(new Vector3(move_y,0, move_x));
+            transform.Translate(new Vector3(move_y, 0, move_x));            
+            */
+
+            rb.MovePosition(transform.position + transform.forward * Time.deltaTime * stepLenght);
+
+            if (lockRotationXZ)
+            {
+                Vector3 eulerAngles = transform.rotation.eulerAngles;
+                eulerAngles = new Vector3(0, eulerAngles.y, 0);
+                transform.rotation = Quaternion.Euler(eulerAngles);
+            }
         }
+   
 	}
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        print("No longer in contact with " + collisionInfo.transform.name);
+    }
+
 }
